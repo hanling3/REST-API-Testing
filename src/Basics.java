217ac2462c2abe08456af9a10994a8d89e3518b7
Hedy Yang <hedyyang1004@gmail.com>
2023-04-17 13:14:03 -0400

    Parse Json response body

--- src/Basics.java
+++ src/Basics.java
@@ -1,39 +1,42 @@
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+//Eclipse does not give auto suggestion for static libraries
 
+import files.payload;
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 
 public class Basics {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
+
         // validate if Add Place API is working as expected
 
-        // given - all input details
-        // when - submit the API - resource, http method
-        // then - validate the response
         RestAssured.baseURI= "https://rahulshettyacademy.com";
-        given().log().all()
+        // given - all input details
+        String response= given().log().all()
             .queryParam("key", "qaclick123")
             .header("Content-Type", "application/json")
-            .body("{\n" +
-                "  \"location\": {\n" +
-                "    \"lat\": -38.383494,\n" +
-                "    \"lng\": 33.427362\n" +
-                "  },\n" +
-                "  \"accuracy\": 50,\n" +
-                "  \"name\": \"Frontline house\",\n" +
-                "  \"phone_number\": \"(+91) 983 893 3937\",\n" +
-                "  \"address\": \"29, side layout, cohen 09\",\n" +
-                "  \"types\": [\n" +
-                "    \"shoe park\",\n" +
-                "    \"shop\"\n" +
-                "  ],\n" +
-                "  \"website\": \"http://google.com\",\n" +
-                "  \"language\": \"French-IN\"\n" +
-                "}\n" +
-                "")
+            .body(payload.AddPlace())
+            // when - submit the API - resource, http method
             .when().post("maps/api/place/add/json")
-            .then().log().all().statusCode(200);
+            // then - validate the response
+            .then()
+            .statusCode(200)
+            .body("scope", equalTo("APP"))
+            .header("server", "Apache/2.4.41 (Ubuntu)")
+            .extract().response().asString();
+
+        System.out.println(response);
+
+        JsonPath js= new JsonPath(response); // for parsing Json
+        String placeId= js.getString("place_id");
+
+        System.out.println(placeId);
+
+        // Add place -> Update place with new address
+        // -> Get place to validate if new address is present in response
 
     }
 
