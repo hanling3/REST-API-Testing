4662b11ed7ce2a6744dee67a593d71ed1dc1f790
Hedy Yang <hedyyang1004@gmail.com>
2023-04-17 12:46:54 -0400

    Validate AddPlace API

--- src/Basics.java
+++ src/Basics.java
@@ -1,7 +1,39 @@
+import static io.restassured.RestAssured.given;
+
+import io.restassured.RestAssured;
+
 public class Basics {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub\
+        // TODO Auto-generated method stub
+        // validate if Add Place API is working as expected
+
+        // given - all input details
+        // when - submit the API - resource, http method
+        // then - validate the response
+        RestAssured.baseURI= "https://rahulshettyacademy.com";
+        given().log().all()
+            .queryParam("key", "qaclick123")
+            .header("Content-Type", "application/json")
+            .body("{\n" +
+                "  \"location\": {\n" +
+                "    \"lat\": -38.383494,\n" +
+                "    \"lng\": 33.427362\n" +
+                "  },\n" +
+                "  \"accuracy\": 50,\n" +
+                "  \"name\": \"Frontline house\",\n" +
+                "  \"phone_number\": \"(+91) 983 893 3937\",\n" +
+                "  \"address\": \"29, side layout, cohen 09\",\n" +
+                "  \"types\": [\n" +
+                "    \"shoe park\",\n" +
+                "    \"shop\"\n" +
+                "  ],\n" +
+                "  \"website\": \"http://google.com\",\n" +
+                "  \"language\": \"French-IN\"\n" +
+                "}\n" +
+                "")
+            .when().post("maps/api/place/add/json")
+            .then().log().all().statusCode(200);
 
     }
 
